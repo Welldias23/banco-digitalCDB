00f15725a1a2daed06972bea41f2f02b4b7ea6e5
Wellington Ribeiro Dias <wellingtond712@gmail.com>
2025-03-26 11:03:39 -0300

    feat(cliente) implementar algumas validações que eram possiveis no clienteDto.

--- src/main/java/com/well/banco_digital_CDBW/dto/ClienteRequest.java
+++ src/main/java/com/well/banco_digital_CDBW/dto/ClienteRequest.java
@@ -4,10 +4,23 @@ import java.time.LocalDate;
 
 import com.well.banco_digital_CDBW.entity.Endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record ClienteRequest(
+		@NotNull(message = "O campo cpf é obrigatorio.")
 		Long cpf,
+		@NotBlank(message = "O campo ome é obrigatorio.")
+		@Size(min = 2, max = 200, message = "o campo nome deve conter no mínimo 2 caracteres e máximo de 100 caracteres.")
+		@Pattern(regexp = "^[a-zA-ZÀ-ú ]+$", message = "O campo nome deve conter apenas letras.")
 		String nome,
+		@NotBlank(message = "O campo senha é obrigatorio.")
+		@Size(min = 6, message = "A senha deve ter no minimo 6 digitos.")
 		String senha,
+		@Past(message = "A data de nascimento deve estar no passado.")
 		LocalDate dataNascimento,
 		Endereco endereco) {
 
