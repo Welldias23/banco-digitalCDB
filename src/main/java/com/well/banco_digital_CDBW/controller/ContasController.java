d7371f76540f2ca0ca2d494f643a472a00d27f48
Wellington Ribeiro Dias <wellingtond712@gmail.com>
2025-04-02 09:49:37 -0300

    refact(Conta) Implementar atribuicão da conta com o id do cliente logado.

--- src/main/java/com/well/banco_digital_CDBW/controller/ContasController.java
+++ src/main/java/com/well/banco_digital_CDBW/controller/ContasController.java
@@ -2,6 +2,7 @@ package com.well.banco_digital_CDBW.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,8 @@ import com.well.banco_digital_CDBW.dto.ClienteAtualizadoDto;
 import com.well.banco_digital_CDBW.dto.ClienteRequest;
 import com.well.banco_digital_CDBW.dto.ContaAAbrir;
 import com.well.banco_digital_CDBW.dto.ContaDTO;
+import com.well.banco_digital_CDBW.entity.Cliente;
+import com.well.banco_digital_CDBW.service.ClienteService;
 import com.well.banco_digital_CDBW.service.ContaService;
 
 import jakarta.validation.Valid;
@@ -26,26 +29,26 @@ public class ContasController {
 	@Autowired
 	private ContaService contaService;
 
-	@PostMapping("/cadastrar/{id}")
+	@PostMapping("/cadastrar")
 	//refatorar esse metodo
-	public ResponseEntity<ContaDTO> cadastrar(@PathVariable Long id, @RequestBody ContaAAbrir contaAAbrir){
-		var conta = contaService.criarConta(id, contaAAbrir);
+	public ResponseEntity<ContaDTO> cadastrar( @RequestBody ContaAAbrir contaAAbrir, @AuthenticationPrincipal Cliente clienteLogado){
+		var conta = contaService.criarConta(clienteLogado.getId(), contaAAbrir);
 		return ResponseEntity.ok(new ContaDTO(conta));	
 	}
 	
-	@GetMapping("/{id}")
+	@GetMapping("/{idConta}")
 	public ResponseEntity<ContaDTO> dados(@PathVariable Long id) {
 		
 		return null;
 	}
 	
-	@PutMapping("/{id}")
+	@PutMapping("/{idConta}")
 	public ResponseEntity<ContaDTO> atualizar(@RequestBody @Valid ClienteAtualizadoDto clienteAtualizar, @PathVariable Long id) {
 		
 		return null;
 	}
 
-	@DeleteMapping("/{id}")
+	@DeleteMapping("/{idConta}")
 	public ResponseEntity<ContaDTO> excluir(@PathVariable Long id) {
 		
 		return null;
