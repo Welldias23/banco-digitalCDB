15817463023ce9ca0537c724d319044286b037b7
wellingtondias <wellingtond712@gmail.com>
2025-05-14 20:03:15 -0300

    feat(pagamento) Cria a mingratiom de pagameto.

---
+++ src/main/resources/db/V7__creat-table-pagamentos.sql
@@ -0,0 +1,11 @@
+CREATE TABLE pagamento (
+	 id SERIAL PRIMARY KEY,
+	 tipo_pagamento VARCHAR(31),
+	 data_pagamento DATE NOT NULL,
+	 hora_pagamento TIME NOT NULL,
+	 nome_estabelecimento VARCHAR(255),
+	 nome_objeto VARCHAR(255),
+	 valor NUMERIC(19, 2),
+	 cartao_credito_id BIGINT NOT NULL REFERENCES cartao(id),
+	 cartao_debito_id BIGINT NOT NULL REFERENCES cartao(id)
+);
