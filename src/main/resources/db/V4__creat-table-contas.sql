a42791372b295d12e488f32960bf28115814c829
wellingtondias <wellingtond712@gmail.com>
2025-05-14 20:06:11 -0300

    feat(conta) Cria a mingratiom de conta.

--- src/main/resources/db/V4__creat-table-contas.sql
+++ src/main/resources/db/V4__creat-table-contas.sql
@@ -1,11 +1,12 @@
 CREATE TABLE conta (
     id SERIAL PRIMARY KEY,
+    tipo_conta VARCHAR(31),
     agencia BIGINT,
     numero_conta VARCHAR(255),
     saldo NUMERIC(19, 2),
     ativa BOOLEAN,
     data_criacao DATE,
     chave_pix VARCHAR(255),
-    cliente_id BIGINT NOT NULL,
-    CONSTRAINT fk_conta_cliente FOREIGN KEY (cliente_id) REFERENCES cliente(id)
+    cliente_id BIGINT NOT NULL REFERENCES cliente(id),
+    rendimento FLOAT
 );
