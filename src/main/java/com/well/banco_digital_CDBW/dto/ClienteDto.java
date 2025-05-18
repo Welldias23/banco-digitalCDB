4f88ba59b04fa3125b0bc906c01cb8f1c1085797
wellingtondias <wellingtond712@gmail.com>
2025-05-17 21:54:39 -0300

    feat(cliente) Implementa melhora na documentacao swagger, com exemplos de dados que devem ser enviados na requisicao.

--- src/main/java/com/well/banco_digital_CDBW/dto/ClienteDto.java
+++ src/main/java/com/well/banco_digital_CDBW/dto/ClienteDto.java
@@ -6,10 +6,8 @@ import java.time.LocalDate;
 import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonView;
-import com.well.banco_digital_CDBW.entity.Cliente;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -18,22 +16,22 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record ClienteDto(
-		@JsonView(View.Resumo.class)
+		@JsonView(View.Detalhar.class)
 		@Schema(accessMode = Schema.AccessMode.READ_ONLY)
 		Long id,
 		
-		@JsonView({View.Resumo.class, View.Persistir.class})
+		@JsonView({View.Detalhar.class, View.Persistir.class})
 		@NotBlank(groups = {Creat.class, UpDate.class}, message = "O campo nome é obrigatorio.")
 		@Size(min = 2, max = 200, message = "o campo nome deve conter no mínimo 2 caracteres e máximo de 100 caracteres.")
 		@Pattern(regexp = "^[a-zA-ZÀ-ú ]+$", message = "O campo nome deve conter apenas letras.")
 		String nome,
 		
-		@JsonView({View.Resumo.class, View.Persistir.class})
+		@JsonView({View.Detalhar.class, View.Persistir.class})
 		@NotBlank(groups = Creat.class, message = "O campo cpf é obrigatorio.")
 		@CPF
 		String cpf,
 		
-		@JsonView({View.Resumo.class, View.Persistir.class})
+		@JsonView({View.Detalhar.class, View.Persistir.class})
 		@NotBlank(groups = {Creat.class, UpDate.class}, message = "O campo email é obrigatorio.")
 		@Email(message = "Deve ser um emai valido.")
 		String email,
@@ -43,12 +41,12 @@ public record ClienteDto(
 		@Size(min = 6, message = "A senha deve ter no minimo 6 digitos.")
 		String senha,
 		
-		@JsonView({View.Resumo.class, View.Persistir.class})
+		@JsonView({View.Detalhar.class, View.Persistir.class})
 		@NotNull(groups = {Creat.class, UpDate.class}, message = "O campo data de nascimento é obrigatorio.")
 		@Past(message = "A data de nascimento deve estar no passado.")
 		LocalDate dataNascimento,
 		
-		@JsonView({View.Resumo.class, View.Persistir.class})
+		@JsonView({View.Detalhar.class, View.Persistir.class})
 		@NotNull(groups = {Creat.class, UpDate.class}, message = "O campo renda mensal é obrigatorio.")
 		BigDecimal rendaMensal) {
 
